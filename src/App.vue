5cc01daf793a7c4d80b6c0f415f3c6d80e69cadc
kerry-okpere <kerryokpere@gmail.com>
2021-05-03 14:13:57 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <Text v-model="state.text"/>
+  ===>{{state.text}}
+  
+</template>
+
+<script setup>
+import Text from './components/Text.vue'
+import { reactive } from 'vue'
+
+const state = reactive({ 
+  text: 'kerry' 
+})
+// This starter template is using Vue 3 experimental <script setup> SFCs
+// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Text.vue
@@ -0,0 +1,24 @@
+<template>
+  <label>
+    Text <br>
+    <input type="text" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)">
+  </label>
+</template>
+
+<script>
+import { reactive, toRefs } from 'vue'
+export default {
+  props:{
+    modelValue: String
+  },
+  emits:['update:modelValue'],
+  setup(props){
+    return{
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
